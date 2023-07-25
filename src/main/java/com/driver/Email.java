c2ec9520e84f11cfb25e7722f68f5d2ed74b2b5b
vasu <pentapativasu3333@gmail.com>
2023-07-25 16:06:11 +0530

    first

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,45 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                System.out.println("Password changed successfully");
+                this.password=newPassword;
+            }
+            else{
+                System.out.println("The new password is not valid");
+            }
+        }
+        else{
+            System.out.println("The given password doesn't match the current password");
+        }
+    }
+    public boolean isValid(String newPassword){
+        Boolean capital=false;
+        Boolean small=false;
+        Boolean digit=false;
+        Boolean special=false;
+        if(newPassword.length()<8){
+            return false;
+        }
+        for(int i=0;i<newPassword.length();i++){
+            char ch=newPassword.charAt(i);
+            if((ch >= 'A')&& (ch <= 'Z')){
+                capital=true;
+            }
+            else if((ch >= 'a')&& (ch <= 'z')){
+                small=true;
+            }
+            else if ((ch >='0') && (ch <= '9')) {
+                digit=true;
+            }
+            else{
+                special=false;
+            }
+        }
+        if(capital && small && digit && special){
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 
 public class Workspace extends Gmail{
@@ -12,12 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+       calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +27,27 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+         if(calendar.size()==0){
+             return 0;
+         }
+         ArrayList<Pair<LocalTime,Integer>> endtimes=new ArrayList<>();
+         for(int i=0;i< calendar.size();i++){
+             endtimes.add(Pair.of(calendar.get(i).getEndTime(),i));
+         }
+        Collections.sort(endtimes);
 
+         LocalTime time_limit=endtimes.get(0).getLeft();
+
+         int count=0;
+         if(!endtimes.isEmpty()){
+             count++;
+         }
+         for(int i=1;i<endtimes.size();i++){
+             if(calendar.get(endtimes.get(i).getRight()).getStartTime().compareTo(time_limit)>0){
+                 count++;
+                 time_limit=endtimes.get(i).getLeft();
+             }
+         }
+       return count;
     }
 }

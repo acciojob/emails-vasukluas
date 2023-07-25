2942b5aa2f9135fce033a55a11eecaa071a9735f
vasu <pentapativasu3333@gmail.com>
2023-07-25 17:21:49 +0530

    adding

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,12 +19,6 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
         if(oldPassword.equals(password)){
             if(isValid(newPassword)){
                 System.out.println("Password changed successfully");
@@ -61,7 +55,7 @@ public class Email {
                 special=false;
             }
         }
-        if(capital && small && digit && special){
+        if(capital && small && digit && special==true){
             return true;
         }
         return false;
